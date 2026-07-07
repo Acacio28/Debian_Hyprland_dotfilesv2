030c7fe1145ba39f8a4aca741d28a4da9f91b247
Akashio28 <acacioelezito933@gmail.com>
2026-07-07 21:50:44 +0900

    fix: monitors.lua use preferred mode for projector compat

---
+++ monitors.lua
@@ -0,0 +1,15 @@
+-- /* ---- 💫 https://github.com/Acacio28 ---- */
+
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "preferred",
+    position = "0x0",
+    scale    = 1.0,
+})
+
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "preferred",
+    position = "0x1080",
+    scale    = 1.0,
+})
